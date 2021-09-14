86b8a6a05444f67b4bb5612a2cfe5c8e1f090fff
Hill <hill@kimo.com>
2021-09-15 02:58:18 +0800

    try ci

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@ DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 do_build() {
   VER=$1
+  DEST_FOLDER=${DIR}/php-$VER
+  mkdir -p ${DEST_FOLDER}
+  cp $DIR/compile.sh ${DEST_FOLDER}
   echo "building --- Version: " $VER "-->";
-  DEST_FILE=${DIR}/php$VER/Dockerfile
+  DEST_FILE=${DEST_FOLDER}/Dockerfile
   cp Dockerfile ${DEST_FILE}
   sed -i -e "s|\[VERSION\]|$VER|g" ${DEST_FILE}
   if [ -e "${DEST_FILE}-e" ]; then rm ${DEST_FILE}-e; fi;
 }
 
-do_build 8
-do_build 5.6
-do_build 7.2 
+do_build $1 
